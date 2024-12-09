7c538626c567a80ae05a74c927b2d780f158768a
MaryKilewe <marykilewe@gmail.com>
2024-12-09 11:26:57 +0300

    test if empty values in required base variables

---
+++ src/store/mapper/mutations.js
@@ -0,0 +1,50 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import {API_URL} from "../../constants";
+
+const saveMappings = async ({baselookup,formData}) => {
+    const res = fetch(`${API_URL}/dictionary_mapper/add_mapped_variables/${baselookup}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+    });
+    // return res;
+};
+
+export const useSaveMappings = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: saveMappings,
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['mappings']})
+        }
+    })
+};
+
+
+
+const testMappings = async ({baselookup,formData}) => {
+    const res = await fetch(`${API_URL}/dictionary_mapper/test/mapped_variables/${baselookup}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+    });
+    const jsonData = await res.json();
+
+    return jsonData?.data ?? [];
+};
+
+export const useTestMappings = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: testMappings,
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['test_mappings']})
+        }
+    })
+};
